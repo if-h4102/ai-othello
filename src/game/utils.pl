61b84819576fd71312edee4aab4664b0e5487797
frobion <frobion.robion@insa-lyon.fr>
2016-11-25 11:16:10 +0100

    add nextCase and nextBoard to utils

--- src/game/utils.pl
+++ src/game/utils.pl
@@ -1,24 +1,47 @@
-:- module(utils, [getVal/3, invertVal/2, isCaseEmpty/2, isOnBoard/2]).
+:- module(utils, [getVal/3, isCaseEmpty/2, isOnBoard/2, nextCase/4]).
 
 getVal(X, Y, Val)  :- 
   board(Board),  
   nth0(X, Board, Column), 
   nth0(Y, Column, Val).
-
-% if case (X, Y) is empty, return false and stop
-invertVal(X, Y) :-
-  isCaseEmpty(X, Y),
-  !, 
-  fail.
-  
-% if case (X, Y) is not empty, invert the color 
-invertVal(X, Y) :-
-  getVal(X, Y, Val),
-  NewVal is -Val,
-  getVal(X, Y, NewVal).
   
 isCaseEmpty(X, Y) :-
   getVal(X, Y, Val),
   var(Val).
 
-isOnBoard(X,Y) :- X > 0, X < 9, Y > 0, Y < 9.
+isOnBoard(X,Y) :- 
+  X > 0, X < 9, Y > 0, Y < 9.
+  
+nextCase(X, Y, NewX, NewY) :-
+  isOnBoard(X, Y),
+  X == 8,
+  Y == 8,
+  !,
+  fail.
+  
+nextCase(X, Y, NewX, NewY) :-
+  isOnBoard(X, Y),
+  X == 8,
+  NewX is 1,
+  NewY is Y + 1.
+  
+nextCase(X, Y, NewX, NewY) :-
+  isOnBoard(X, Y),
+  NewX is X + 1,
+  NewY is Y.
+
+nextBoard(OldBoard, NewBoard) :-
+  nextBoard(OldBoard, NewBoard, 1, 1).
+  
+% Private Method
+getVal(X, Y, Board, Val)  :-  
+  nth0(X, Board, Column), 
+  nth0(Y, Column, Val).
+  
+nextBoard(OldBoard, NewBoard, X, Y) :-
+  getVal(X, Y, NewBoard, NewVal),
+  var(NewVal),
+  getVal(X, Y, OldBoard, OldVar),
+  getVal(X, Y, NewBoard, OldVar),
+  nextCase(X, Y, NewX, NewY),
+  nextBoard(OldBoard, NewBoard, NewX, NewY).
