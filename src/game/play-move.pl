4358ab131d8b22964e8e1eb91c40a1c1d86eb027
frobion <frobion.robion@insa-lyon.fr>
2016-11-25 09:49:47 +0100

    temp

---
+++ src/game/play-move.pl
@@ -0,0 +1,4 @@
+:- module(play-move, [test/0]).
+
+playMove(X, Y, NextBoard) :-
+  board(Board).
---
+++ src/io/user.pl
@@ -0,0 +1,11 @@
+:- module(user, [readIntInput/2]).
+
+
+readIntInput(X) :-
+    repeat, write(' Please enter an integer '), read(X), integer(X), !.
+
+readIntInput(X, Y) :- 
+  write('X ?'),
+  readIntInput(X),
+  write('Y ?'),
+  readIntInput(Y).
