ae4ee54987b2829d2b7d2db318d2a39ae9fe73b7
Sn0wFox <ruben.pericas@laposte.net>
2016-11-26 20:26:00 +0100

    Test: added API endpoint to retrieve squared number

--- src/server.pl
+++ src/server.pl
@@ -1,5 +1,6 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_parameters)).
 
 % Add a basic route
 :- http_handler(/, welcome, []).
@@ -11,3 +12,15 @@ welcome(_Request) :-
 
 % This predicate allows us to launch the server on the port given as parameter
 server(Port) :- http_server(http_dispatch, [port(Port)]).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+% Let's try to build an API endpoint,
+% which send back the square number of the given X
+square(X, Res) :- Res is X * X .
+api_square(Request) :-
+    http_parameters(Request, [number(Number, [number])]),
+    format('Content-type: text/plain~n~n'),
+    square(Number, Res),
+    format(Res).
+:- http_handler('/square', api_square, []).
