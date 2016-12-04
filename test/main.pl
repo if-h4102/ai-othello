8ddf569f8573bbdd17242e67691a87d95fe5865b
Sn0wFox <ruben.pericas@laposte.net>
2016-12-04 18:32:33 +0100

    Added tests for random-ai in CI

--- test/main.pl
+++ test/main.pl
@@ -1,6 +1,10 @@
 :- use_module(library(plunit)).
 :- use_module('score', []).
+:- use_module('random-ai', []).
 
 % Run all tests
-:- run_tests(score), halt(0).	% Exit with 0 when all tests passed
-:- halt(1).						% Or exit with 1 when at least one test failed
+:- run_tests([
+    score,
+    random_ai
+  ]), halt(0).	          % Exit with 0 when all tests passed
+:- halt(1).						    % Or exit with 1 when at least one test failed
