566a315a9cf910aba0d6d009c90112461d1da0aa
frobion <francois.robion@insa-lyon.fr>
2016-12-04 21:14:35 +0100

    Debug IA2

--- src/ai/basic-ai.pl
+++ src/ai/basic-ai.pl
@@ -5,67 +5,75 @@
 
 %testBoard(Board) :- 
 %    Board =[
-%    [_, _, _, _, _, _, _, _, _, _],
-%    [_, _, _, _, _, _, _, _, _, _],
-%    [_, _, _, _, _, _, _, _, _, _],
-%    [_, _, _, _, _, _, _, _, _, _],
-%    [_, _, _, _,-1, 1, _, _, _, _],
-%    [_, _, _, _, 1,-1, _, _, _, _],
-%    [_, _, _, _, _, _, _, _, _, _],
-%    [_, _, _, _, _, _, _, _, _, _],
-%    [_, _, _, _, _, _, _, _, _, _],
+%    [_, _, _, _, _, _, _, _, _, _], 
+%    [_, _, _, _, _, _, _, _, _, _], 
+%    [_, _, _, _, _, _, _, _, _, _], 
+%    [_, _, _, _, _, _, _, _, _, _], 
+%    [_, _, _, _, -1, 1, _, _, _, _], 
+%    [_, _, _, _, 1, -1, _, _, _, _], 
+%    [_, _, _, _, _, _, _, _, _, _], 
+%    [_, _, _, _, _, _, _, _, _, _], 
+%    [_, _, _, _, _, _, _, _, _, _], 
 %    [_, _, _, _, _, _, _, _, _, _]
 %    ].
 
 
-scoreCoefBoard(ScoreBoard) :- ScoreBoard =  [[1000 ,-30 ,10 ,10 ,10 ,10 ,10 , 10 ,-30 ,1000 ],
-                                            [-30   ,-50 ,-5 ,-5 ,-5 ,-5 ,-5 ,-5  ,-50 ,-30  ],
-                                            [10    ,-5  ,1  ,1  ,1  ,1  ,1  ,1   ,-5  ,10   ],
-                                            [10    ,-5  ,1  ,1  ,1  ,1  ,1  ,1   ,-5  ,10   ],
-                                            [10    ,-5  ,1  ,1  ,1  ,1  ,1  ,1   ,-5  ,10   ],
-                                            [10    ,-5  ,1  ,1  ,1  ,1  ,1  ,1   ,-5  ,10   ],
-                                            [10    ,-5  ,1  ,1  ,1  ,1  ,1  ,1   ,-5  ,10   ],
-                                            [10    ,-5  ,1  ,1  ,1  ,1  ,1  ,1   ,-5  ,10   ],
-                                            [-30   ,-50 ,-5 ,-5 ,-5 ,-5 ,-5 ,-5  ,-50 ,-30  ],
-                                            [1000  ,-30 ,10 ,10 ,10 ,10 ,10 , 10 ,-30 ,1000 ]].
+scoreCoefBoard(ScoreBoard) :- ScoreBoard =  [[1000 , -30 , 10 , 10 , 10 , 10 , 10 , 10 , -30 , 1000 ], 
+                                            [-30   , -50 , -5 , -5 , -5 , -5 , -5 , -5  , -50 , -30  ], 
+                                            [10    , -5  , 1  , 1  , 1  , 1  , 1  , 1   , -5  , 10   ], 
+                                            [10    , -5  , 1  , 1  , 1  , 1  , 1  , 1   , -5  , 10   ], 
+                                            [10    , -5  , 1  , 1  , 1  , 1  , 1  , 1   , -5  , 10   ], 
+                                            [10    , -5  , 1  , 1  , 1  , 1  , 1  , 1   , -5  , 10   ], 
+                                            [10    , -5  , 1  , 1  , 1  , 1  , 1  , 1   , -5  , 10   ], 
+                                            [10    , -5  , 1  , 1  , 1  , 1  , 1  , 1   , -5  , 10   ], 
+                                            [-30   , -50 , -5 , -5 , -5 , -5 , -5 , -5  , -50 , -30  ], 
+                                            [1000  , -30 , 10 , 10 , 10 , 10 , 10 , 10 , -30 , 1000 ]].
 
-getScoreBoard(Board,Score) :- getScoreBoard(Board,Score,1).
-getScoreBoard(Board,Score,Player) :- getScoreBoard(Board,PlayerIndependantScore,1,1), Score is PlayerIndependantScore*Player.
+getScoreBoard(Board, Score) :- getScoreBoard(Board, Score, 1).
+getScoreBoard(Board, Score, Player) :- getScoreBoard(Board, PlayerIndependantScore, 1, 1), Score is PlayerIndependantScore*Player.
 
-getScoreBoard(_,Score,8,9) :- Score is 0,!.
+getScoreBoard(_, Score, 8, 9) :- Score is 0, !.
 %% if we try to go too far on Y increment X
-getScoreBoard(Board,Score,LastX,9) :- X is LastX+1, getScoreBoard(Board,Score,X,1),!.
+getScoreBoard(Board, Score, LastX, 9) :- X is LastX+1, getScoreBoard(Board, Score, X, 1), !.
 %% if the case is unset, score don't change
-getScoreBoard(Board,Score,LastX,LastY) :- utils:getVal(Board,LastX,LastY,Case), var(Case), Y is LastY+1, getScoreBoard(Board, Score, LastX, Y),!.
+getScoreBoard(Board, Score, LastX, LastY) :- utils:getVal(Board, LastX, LastY, Case), var(Case), Y is LastY+1, getScoreBoard(Board, Score, LastX, Y), !.
 %% else add the case value to the currant score
-getScoreBoard(Board,Score,LastX,LastY) :- utils:getVal(Board,LastX,LastY,Case), Y is LastY+1, getScoreBoard(Board, OldScore, LastX, Y), Score is OldScore+Case,!.
+getScoreBoard(Board, Score, LastX, LastY) :- utils:getVal(Board, LastX, LastY, Case), Y is LastY+1, getScoreBoard(Board, OldScore, LastX, Y), Score is OldScore+Case, !.
 
-bestMove(Board,X,Y,Player) :- utils_ai:possibleMoves(Board,Player,PossibleMoves), foundBestMove(Board,PossibleMoves,-99999,X,Y,Player), !.
+bestMove(Board, X, Y, Player) :- utils_ai:possibleMoves(Board, Player, PossibleMoves), foundBestMove(Board, PossibleMoves, -99999, X, Y, Player), !.
 
-%foundBestMove(+Board, +MoveList, +BestScore, BestX, BestY, +Player)
-foundBestMove(_,[],_,_,_,_) :- format(user_output, 'end', []), !.
+%foundBestMove(+Board, +MoveList, +BestScore, -BestX, -BestY, +Player)
+foundBestMove(_, [], _, _, _, _) :- format(user_output, 'end ai2~n', []), !.
 
-foundBestMove(Board,[[X|Y]|Tail], BestScore, BestX, BestY, Player) :-
-    utils:updateBoard(Board, Player, X, Y, NewBoard),
-    getScoreBoard(NewBoard, Score, Player),
-    Score > BestScore,
-    foundBestMove(Board,Tail,Score,NewBestX,NewBestY,Player),
+foundBestMove(Board, [Move|Tail], BestScore, BestX, BestY, Player) :-
+    utils_ai:getXYMove(Move, X, Y), 
+    utils:updateBoard(Board, Player, X, Y, NewBoard), 
+    getScoreBoard(NewBoard, Score, Player), 
+    Score > BestScore, 
+    foundBestMove(Board, Tail, Score, NewBestX, NewBestY, Player), 
     ( % if a better score is found set return var with it else use the current one
         ( 
             var(NewBestX), 
             var(NewBestY), 
-            BestX is NewBestX, 
-            BestY is NewBestY )
+            writeln(1),
+            BestX is X,
+            writeln(2), 
+            BestY is Y,
+            writeln(3)
+        )
         ;
         ( 
-            BestX is X,
-            BestY is Y 
+            writeln(4),
+            BestX is NewBestX,
+            writeln(5),
+            BestY is NewBestY,
+            writeln(6) 
         )
-    ),
+    ), 
     !.
 
-foundBestMove(Board,[_|Tail], BestScore, BestX, BestY, Player) :- 
-    foundBestMove(Board, Tail, BestScore, BestX, BestY, Player),
+foundBestMove(Board, [_|Tail], BestScore, BestX, BestY, Player) :- 
+    foundBestMove(Board, Tail, BestScore, BestX, BestY, Player), 
     !.
     
 
