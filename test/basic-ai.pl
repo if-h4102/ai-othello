1a640793dd4373a98b1959d6d311201d6192013a
frobion <francois.robion@insa-lyon.fr>
2016-12-04 21:23:03 +0100

    add tests for basic_ai

--- test/basic-ai.pl
+++ test/basic-ai.pl
@@ -1,6 +1,6 @@
 :- module('test_basic_ai', []).
 :- use_module(library(plunit)).
-:- use_module('../src/ai/basic-ai', []).
+:- use_module('../src/ai/ai', []).
 
 % To run this test case
 test_basic_ai :-
@@ -8,5 +8,20 @@ test_basic_ai :-
 
 % Define the test case
 :- begin_tests(basic_ai).  
+
+test(bestMove) :-
+    Board = [[ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, 1,-1,-1, _, _, _],
+             [ _, _, _, _, _, 1, 1, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _]],
+    ai:bestMove(Board, 1, 2, X, Y),
+    X == 4,
+    Y == 7.
 
 :- end_tests(basic_ai).
