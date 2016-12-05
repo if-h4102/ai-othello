24c1d3af3aa987930475b9572281878cd4b07be1
HugoMoy <hugo.moynac@gmail.com>
2016-12-05 13:23:27 +0100

    basic ai tests with corner

--- test/basic-ai.pl
+++ test/basic-ai.pl
@@ -24,4 +24,38 @@ test(bestMove) :-
     X == 4,
     Y == 7.
 
+% test where ia must not choose the highest amount of case to swap (case before corner).
+testNotCorner(bestMove) :-
+    Board = [[ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, 1, _, _, _],
+             [ _, _, _, _, _, 1, _, _, _, _],
+             [ _, _, _, _,-1, _, _, _, _, _],
+             [ _, _, _, 1, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _]],
+    ai:bestMove(Board, 1, 2, X, Y),
+    X==7,
+    Y==3.
+
+
+% test where ia must not choose the highest amount of case to swap (corner is possible).
+testCorner(bestMove) :-
+    Board = [[ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, 1, _, _],
+             [ _, _, _, _, _, _,-1, _, _, _],
+             [ _, _, _, _, _, _, 1, _, _, _],
+             [ _, _, _, _, _, _, 1, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _]],
+    ai:bestMove(Board, 1, 2, X, Y),
+    X==1,
+    Y==8.
+
+
 :- end_tests(basic_ai).
