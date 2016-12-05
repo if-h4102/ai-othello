4e6c18fe0d8efe5b00c925a89c60b544b72a776d
frobion <francois.robion@insa-lyon.fr>
2016-12-05 13:08:00 +0100

    Fix tests

--- src/ai/basic-ai.pl
+++ src/ai/basic-ai.pl
@@ -18,79 +18,7 @@
 %    ].
 
 
-scoreCoefBoard(ScoreBoard) :- ScoreBoard = [[0, 0   , 0  , 0  , 0  , 0  , 0  , 0  , 0   , 0], 
-                                            [0, 1000, -30, 100, 100, 100, 100, -30, 1000, 0], 
-                                            [0, -30 , -50, -5 , -5 , -5 , -5 , -50, -30 , 0], 
-                                            [0, 100 , -5 , 1  , 1  , 1  , 1  , -5 , 100 , 0], 
-                                            [0, 100 , -5 , 1  , 1  , 1  , 1  , -5 , 100 , 0], 
-                                            [0, 100 , -5 , 1  , 1  , 1  , 1  , -5 , 100 , 0], 
-                                            [0, 100 , -5 , 1  , 1  , 1  , 1  , -5 , 100 , 0], 
-                                            [0, -30 , -50, -5 , -5 , -5 , -5 , -50, -30 , 0], 
-                                            [0, 1000, -30, 100, 100, 100, 100, -30, 1000, 0], 
-                                            [0, 0   , 0  , 0  , 0  , 0  , 0  , 0  , 0   , 0]].
 
-getScoreBoard(Board, Score, Player) :- 
-    getScoreBoard(Board, PlayerIndependantScore, 1, 1), 
-    Score is PlayerIndependantScore * Player.
-
-getScoreBoard(_, Score, 8, 9) :- 
-    Score is 0, 
-    !.
-
-%% if we try to go too far on Y increment X
-getScoreBoard(Board, Score, LastX, 9) :- 
-    X is LastX+1, 
-    getScoreBoard(Board, Score, X, 1), 
-    !.
-
-%% if the case is unset, score don't change
-getScoreBoard(Board, Score, LastX, LastY) :- 
-    utils:getVal(Board, LastX, LastY, Case), 
-    var(Case),
-    Y is LastY+1, 
-    getScoreBoard(Board, Score, LastX, Y),
-    !.
-
-%% else add the case value to the currant score
-getScoreBoard(Board, Score, LastX, LastY) :- 
-    utils:getVal(Board, LastX, LastY, Case), 
-    Y is LastY+1, 
-    getScoreBoard(Board, OldScore, LastX, Y),
-    scoreCoefBoard(Coef),
-    utils:getVal(Coef, LastX, LastY, CellCoef),
-    Score is OldScore + Case * CellCoef, !.
-
-
-
-getScoreBoard2(Board, Score, Player) :-
-    getScoreBoard2(Board, Player, 1, 1, IndependantPlayerScore),
-    Score is IndependantPlayerScore * Player. % By multipling by player, the ai must always maximise the score.
-
-getScoreBoard2(_, _, 1, 9, Score) :- 
-    Score is 0,
-    !.
-
-getScoreBoard2(Board, Player, X, Y, Score) :-
-    getScoreCell(Board, X, Y, ScoreCell),
-    utils:nextCase(X, Y, NewX, NewY),
-    getScoreBoard2(Board, Player, NewX, NewY, LeftOverScore),
-    Score is ScoreCell + LeftOverScore,
-    !.
-
-% getScoreCell(+Board, +X, +Y, -Score)
-% returns the score associated with this cell, which is 0 is the cell is empty
-% else Player*CoefCell (with Player which is 1 or -1, and CoefCell the coef of the cell, provided by scoreCoefBoard). 
-getScoreCell(Board, X, Y, Score) :-
-    utils:getVal(Board, X, Y, Val),
-    var(Val),
-    Score is 0,
-    !.
-
-getScoreCell(Board, X, Y, Score) :-
-    scoreCoefBoard(Coef),
-    utils:getVal(Board, X, Y, Val),
-    utils:getVal(Coef, X, Y, CoefCell),
-    Score is Val * CoefCell.
 
 bestMove(Board, X, Y, Player) :- utils_ai:possibleMoves(Board, Player, PossibleMoves), foundBestMove(Board, PossibleMoves, -99999, X, Y, Player), !.
 
@@ -100,7 +28,7 @@ foundBestMove(_, [], _, _, _, _) :- format(user_output, 'end ai2~n', []), !.
 foundBestMove(Board, [Move|Tail], BestScore, BestX, BestY, Player) :-
     utils_ai:getXYMove(Move, X, Y), 
     utils:updateBoard(Board, Player, X, Y, NewBoard), 
-    getScoreBoard(NewBoard, Score, Player), 
+    utils_ai:getScoreBoard(NewBoard, Player, Score), 
     Score > BestScore, 
     foundBestMove(Board, Tail, Score, NewBestX, NewBestY, Player), 
     ( % if a better score is found set return var with it else use the current one
--- test/utils_ai.pl
+++ test/utils_ai.pl
@@ -2,6 +2,7 @@
 :- use_module(library(plunit)).
 :- use_module('../src/ai/utils_ai', []).
 
+
 % To run this test case
 test_utils_ai :-
 	run_tests([utils_ai]).
@@ -35,5 +36,29 @@ test(possible_moves) :-
              [ _, _, _, _, _, _, _, _, _, _],
              [ _, _, _, _, _, _, _, _, _, _]],
     utils_ai:possibleMoves(Board, 1, [[3, 5], [4, 6]]).
+
+test(scoreCase) :-
+    Board = [[ _, _, _, _, _, _, _, _, _, _],
+             [ _,-1, 1, -1, _, _, _, _, _, _],
+             [ _,-1, 1, 1, _, _, _, _, _, _],
+             [ _, 1,-1, _, _, _, _, _, _, _],
+             [ _, _, _, _,-1, 1, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _],
+             [ _, _,-1, _, _, _, _, _, _, _],
+             [ _, 1, _, _, _, _, _, _, _, _],
+             [ _, _, _, _, _, _, _, _, _, _]],
+    utils_ai:getScoreCell(Board, 1, 1, Score1),  Score1  == -1000,
+    utils_ai:getScoreCell(Board, 2, 1, Score2),  Score2  == 30,
+    utils_ai:getScoreCell(Board, 3, 1, Score3),  Score3  == 100,
+    utils_ai:getScoreCell(Board, 8, 1, Score4),  Score4  == 1000,
+    utils_ai:getScoreCell(Board, 1, 2, Score5),  Score5  == -30,
+    utils_ai:getScoreCell(Board, 2, 2, Score6),  Score6  == -50,
+    utils_ai:getScoreCell(Board, 3, 2, Score7),  Score7  == 5,
+    utils_ai:getScoreCell(Board, 7, 2, Score8),  Score8  == 50,
+    utils_ai:getScoreCell(Board, 1, 3, Score9),  Score9  == -100,
+    utils_ai:getScoreCell(Board, 2, 3, Score10), Score10 == -5,
+    utils_ai:getScoreCell(Board, 4, 4, Score11), Score11 == -1,
+    utils_ai:getScoreCell(Board, 4, 5, Score12), Score12 == 1.
 
 :- end_tests(utils_ai).
