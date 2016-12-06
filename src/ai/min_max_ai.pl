ef782fd0987b41ffb3d6276c617ff9aaa41022fc
Nicolas <nicolas.six@insa-lyon.fr>
2016-12-06 10:08:25 +0100

    debug of min max AI

--- src/ai/min_max_ai.pl
+++ src/ai/min_max_ai.pl
@@ -33,8 +33,8 @@ findBestMove(Board, [Move|Tail], BestScore, BestX, BestY, AiPlayer) :-
     utils_ai:getXYMove(Move, X, Y), 
     utils:updateBoard(Board, AiPlayer, X, Y, NewBoard),
     HumanPlayer is -AiPlayer, 
-    getScoreMinMax(NewBoard, HumanPlayer, 3, PlayerIndependantScore),
-    Score is PlayerIndependantScore * HumanPlayer, 
+    getScoreMinMax(NewBoard, HumanPlayer, 1, PlayerIndependantScore),
+    Score is PlayerIndependantScore * AiPlayer, 
     Score > BestScore, 
     findBestMove(Board, Tail, Score, NewBestX, NewBestY, AiPlayer), 
     ( % if a better score is found set return var with it else use the current one
@@ -57,14 +57,15 @@ findBestMove(Board, [_|Tail], BestScore, BestX, BestY, Player) :-
     !.
     
     
-getScoreMinMax(Board, Player, 0, Score) :-
+getScoreMinMax(Board, Player, 0, PlayerIndependantScore) :-
     utils_ai:getScoreBoard(Board, Player, ScoreDependantPlayer),
-    Score is ScoreDependantPlayer * Player,
+    PlayerIndependantScore is ScoreDependantPlayer * Player,
     !.
     
-getScoreMinMax(Board, CurrentPlayer, Depth, Score) :-
+getScoreMinMax(Board, CurrentPlayer, Depth, PlayerIndependantScore) :-
     utils_ai:possibleMoves(Board, CurrentPlayer, MoveList),
-    findBestScore(Board, MoveList, CurrentPlayer, Depth, -999999, Score),
+    findBestScore(Board, MoveList, CurrentPlayer, Depth, -999999, FoundScore),
+    PlayerIndependantScore is FoundScore * CurrentPlayer,
     !.
     
 
@@ -81,7 +82,7 @@ findBestScore(Board, [Move|Tail], CurrentPlayer, Depth, CurrentBestScore, FoundB
     NewDepth is Depth - 1,
     NextPlayer is -CurrentPlayer,
     getScoreMinMax(NewBoard, NextPlayer, NewDepth, PlayerIndependantScore),
-    Score is PlayerIndependantScore * NextPlayer,
+    Score is PlayerIndependantScore * CurrentPlayer,
     Score > CurrentBestScore, 
     findBestScore(Board, Tail, CurrentPlayer, Depth, Score, FoundBestScore),
     !.
